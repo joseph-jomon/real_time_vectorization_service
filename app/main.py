7fc2c9122af87a055bcabfa3c807af5fc7bf2c77
joseph-jomon <josephjomon401@gmail.com>
2024-09-01 09:41:50 +0200

    added app.state instead of using global variables , without which variables in lifespan will not be accessible inside the app

--- app/main.py
+++ app/main.py
@@ -12,8 +12,12 @@ async def lifespan(app: FastAPI):
     #Load the ML model
     text_vectorizer = TextVectorizer()# This will load the ai model as soon as the instance of the class is created
     image_vectorizer = ImageVectorizer()
-    yield
-    print("code to release resources")
+    # Store the vectorizers in app.state
+    app.state.text_vectorizer = text_vectorizer
+    app.state.image_vectorizer = image_vectorizer
+    yield # Allow the app to run
+
+    #Clean up the ML models and release the resources
     image_vectorizer.close()
     text_vectorizer.close()
     # Clean up the ML models and release the resources
@@ -26,6 +30,8 @@ class TextInput(BaseModel):
 
 @app.post("/vectorize/text")
 async def vectorize_text(input: TextInput):
+    # Access the model from app.state
+    text_vectorizer = app.state.text_vectorizer
     result = text_vectorizer.vectorize(input.text)
     return result
 
@@ -34,6 +40,6 @@ async def vectorize_image(file: UploadFile = File(...)):
     # Read the image file
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes))
-    
+    image_vectorizer = app.state.image_vectorizer
     result = image_vectorizer.vectorize(image)
     return result
