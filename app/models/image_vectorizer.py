f1b1cd2f9dd34688b048ded461b11348fdb2de7a
joseph-jomon <josephjomon401@gmail.com>
2025-03-01 20:06:52 +0100

    update default model name in ImageVectorizer to StreetCLIP

--- app/models/image_vectorizer.py
+++ app/models/image_vectorizer.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 
 
 class ImageVectorizer:
-    def __init__(self, model_name: str = "openai/clip-vit-base-patch32"): # models are loaded in the __init__ of the class. that means This will load the model as soon as the instance is created
+    def __init__(self, model_name: str = "geolocal/StreetCLIP"): # models are loaded in the __init__ of the class. that means This will load the model as soon as the instance is created
         self.model_name = model_name
         self.vision_model = CLIPVisionModelWithProjection.from_pretrained(model_name) #model loading takes place here
         self.extractor = AutoFeatureExtractor.from_pretrained(model_name) # model loading takes place here also
